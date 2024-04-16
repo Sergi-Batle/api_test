b62380c2b2b1d6ebbbcb769cb0699ff3ae87afe2
fumanji420 <sergibatle00@gmail.com>
2024-04-16 16:15:38 +0200

    v3

--- scripts.js
+++ scripts.js
@@ -1,52 +1,45 @@
-var files_to_ingest = [];
-var ingested_files = [];
-var selected_files = [];
-var relations;
 const ip = '127.0.0.1';
 const port = '8001';
 const url = `${ip}:${port}`;
+var files_to_ingest = [];
+var file_names_to_ingest = [];
+var ingested_files = [];
+var selected_files = [];
 
 
 function selectFile() {
     var fileInput = document.getElementById('fileInput');
     openFile(fileInput);
+    fileInput.value = null;
 }
-
 
 function openFile(fileInput) {
     var ingested = [];
     var check = false;
-    // Verificar si se ha seleccionado un archivo
     if (fileInput.files.length > 0) {
-        // Obtener todos los archivos seleccionados
         var files = fileInput.files;
-
-        // Iterar sobre todos los archivos seleccionados
-        for (var i = 0; i < files.length; i++) {
-            var file = files[i];
-            console.log('files_to_ingest:', files_to_ingest);
-            if (files_to_ingest.some(existingFile => existingFile.name === file.name)) {
-                alert(`${file.name} ya ha sido seleccionado`);
-            }
-            else if (ingested_files.includes(file.name)) {
-                ingested.push(file.name);
+        for (var file of files) {
+            var file_name = file.name;
+            if (ingested_files.includes(file_name)) {
+                ingested.push(file_name);
                 check = true;
+            } else if (file_names_to_ingest.includes(file_name)) {
+                continue;
             } else {
+                file_names_to_ingest.push(file_name);
                 files_to_ingest.push(file);
             }
-
         }
-
-        // Llamar a la función para actualizar la lista de archivos para cargar
         setFilesToIngest();
     }
+
     if (check) {
-        var names = ''
+        var already_ingested = '';
         if (ingested.length > 1) {
             for (var i = 0; i < ingested.length; i++) {
-                names += ingested[i] + ', ';
+                already_ingested += ingested[i] + ', ';
                 if (i == ingested.length - 1) {
-                    names += `${ingested[i]} ya han sido procesados `
+                    already_ingested += `${ingested[i]} ya han sido procesados `
                 }
             }
             alert(`Los archivos ${names}`)
@@ -58,16 +51,11 @@ function openFile(fileInput) {
 
 
 function setFilesToIngest() {
-    // Obtener el elemento de lista de archivos para cargar
     var fileListPre = document.getElementById('file-list-pre');
-
-    // Limpiar la lista
     fileListPre.innerHTML = '';
-
-    // Recorrer la lista de archivos para ingestión y agregar los nombres a la lista HTML
-    files_to_ingest.forEach(function (file) {
+    file_names_to_ingest.forEach(function (name) {
         var listItem = document.createElement('li');
-        listItem.textContent = file.name;
+        listItem.textContent = name;
         listItem.className = 'pl-4 p-2 no-collapse'
         fileListPre.appendChild(listItem);
     });
@@ -78,34 +66,44 @@ function resetPreFiles() {
     var fileListPre = document.getElementById('file-list-pre');
     fileListPre.innerHTML = '';
     files_to_ingest = [];
+    file_names_to_ingest = [];
 }
 
 
 async function ingestFiles() {
+    var loading = document.getElementById('loading');
     if (files_to_ingest.length == 0) {
         alert('No hay archivos para subir')
     } else {
-        for (const file of files_to_ingest) {
-            try {
-                const formData = new FormData();
-                formData.append('file', file);
-
-                const response = await fetch(`http://${url}/v1/ingest/file`, {
-                    method: 'POST',
-                    body: formData
-                });
-
-                if (response.ok) {
-                    const responseData = await response.json();
-                    console.log(responseData);
-                    getIngestedFiles();
-                } else {
-                    console.error('Error al enviar el archivo:', response.status);
-                }
-            } catch (error) {
-                console.error('Error al procesar la solicitud:', error);
-            }
+        for (var file of files_to_ingest) {
+            loading.style.display = 'flex';
+            ingestFile(file, loading);
         }
+    }
+}
+
+
+async function ingestFile(file, loading) {
+    try {
+        const formData = new FormData();
+        formData.append('file', file);
+
+        const response = await fetch(`http://${url}/v1/ingest/file`, {
+            method: 'POST',
+            body: formData
+        });
+        
+        if (response.ok) {
+            const responseData = await response.json();
+            console.log(responseData);
+            getIngestedFiles();
+            loading.style.display = 'none';
+        } else {
+            loading.style.display = 'none';
+            console.error('Error al enviar el archivo:', response.status);
+        }
+    } catch (error) {
+        console.error('Error al procesar la solicitud:', error);
     }
 }
 
@@ -123,11 +121,10 @@ async function getIngestedFiles() {
             throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        relations = relacionarDocId(JSON.stringify(data.data));
-        data.data.forEach(doc => {
-            var name = doc.doc_metadata.file_name;
-            if (!(ingested_files.includes(name))) {
-                ingested_files.push(doc.doc_metadata.file_name);
+        console.log(data);
+        data.forEach(function (fileName) {
+            if (!(ingested_files.includes(fileName))) {
+                ingested_files.push(fileName);
             }
         });
         setIngestedFiles();
@@ -151,28 +148,7 @@ function setIngestedFiles() {
 
     });
     fileListElement.appendChild(fragment);
-    console.log(relations)
-    console.log('Ingested:', ingested_files);
-}
-
-
-function relacionarDocId(jsonData) {
-    const data = JSON.parse(jsonData);
-    const relacionDocId = {};
-
-    data.forEach(obj => {
-        const fileName = obj.doc_metadata.file_name;
-        const docId = obj.doc_id;
-
-        if (fileName in relacionDocId) {
-            relacionDocId[fileName].push(docId);
-        }
-        else {
-            relacionDocId[fileName] = [docId];
-        }
-    });
-
-    return relacionDocId;
+    ingested_files = [];
 }
 
 
@@ -201,34 +177,6 @@ async function keepAlive() {
 }
 
 
-function addToSelectedList(selectElement) {
-    var selectedValue = selectElement.value;
-    var selectedList = document.getElementById('file-list-selected');
-    var selectedText = selectElement.options[selectElement.selectedIndex].textContent;
-
-    // Verificamos si el valor ya está presente en la lista
-    var alreadyExists = false;
-    var listItems = selectedList.getElementsByTagName('li');
-    for (var j = 0; j < listItems.length; j++) {
-        if (listItems[j].getAttribute('value') === selectedValue) {
-            alreadyExists = true;
-            break;
-        }
-    }
-
-    // Si el valor no está presente, lo agregamos a la lista
-    if (!alreadyExists) {
-        selected_files.push(selectedValue);
-        var newListItem = document.createElement('li');
-        newListItem.textContent = selectedText;
-        newListItem.className = 'pl-4 p-2 border no-collapse';
-        newListItem.setAttribute('value', selectedValue);
-        selectedList.appendChild(newListItem);
-    }
-    selectElement.value = '';
-}
-
-
 function clearSelectedList() {
     selected_files = [];
     var selectedList = document.getElementById('file-list-selected');
@@ -236,88 +184,73 @@ function clearSelectedList() {
 }
 
 
-function selectAllOptions() {
-    selected_files = ingested_files;
-    var select = document.getElementById('file-list-ingested');
+function addToSelectedList(selectElement) {
     var selectedList = document.getElementById('file-list-selected');
-
-    // Recorremos todas las opciones del select
-    for (var i = 0; i < select.options.length; i++) {
-        var option = select.options[i];
-        var optionValue = option.value;
-        var optionText = option.textContent;
-
-        // Verificamos si el valor ya está presente en la lista
-        var alreadyExists = false;
-        var listItems = selectedList.getElementsByTagName('li');
-        for (var j = 0; j < listItems.length; j++) {
-            if (listItems[j].getAttribute('value') === optionValue) {
-                alreadyExists = true;
-                break;
-            }
-        }
-
-        // Si el valor no está presente, lo agregamos a la lista
-        if (!alreadyExists) {
+    var selected = Array.from(selectElement.options)
+        .filter(function (option) {
+            return option.selected;
+        })
+        .map(function (option) {
+            return option.value;
+        });
+    selected.forEach(function (value) {
+        if (!selected_files.includes(value)) {
+            selected_files.push(value);
             var newListItem = document.createElement('li');
-            newListItem.textContent = optionText;
+            newListItem.textContent = value;
             newListItem.className = 'pl-4 p-2 border no-collapse';
-            newListItem.setAttribute('value', optionValue);
+            newListItem.setAttribute('value', value);
             selectedList.appendChild(newListItem);
         }
-    }
+    });
+    selectElement.value = '';
+}
+
+
+function selectAllOptions() {
+    var selectedList = document.getElementById('file-list-selected');
+    ingested_files.forEach(function (value) {
+        if (!selected_files.includes(value)) {
+            selected_files.push(value);
+            var newListItem = document.createElement('li');
+            newListItem.textContent = value;
+            newListItem.className = 'pl-4 p-2 border no-collapse';
+            newListItem.setAttribute('value', value);
+            selectedList.appendChild(newListItem);
+        }
+    });
 }
 
 
 async function deleteFiles() {
+    var deleting = document.getElementById('deleting');
     if (selected_files.length === 0) {
         alert('No hay archivos seleccionados');
+        confirmDelete();
     } else {
-        for (const fileName in relations) {
-            if (selected_files.includes(fileName)) {
-                console.log(`Archivo: ${fileName}`);
-                var ids = relations[fileName];
-                console.log(`IDs:`);
-                ids.forEach(id => {
-                    console.log(id);
-                });
-                deleteFile(ids);
-            }
+        for (var fileName of selected_files) {
+            deleting.style.display = 'block';
+            deleteFile(fileName, deleting)
         }
     }
-    updateIngestedFiles();
     clearSelectedList();
 }
 
 
-function updateIngestedFiles() {
-    ingested_files.forEach(function(ingestedFile, ingestedIndex) {
-        selected_files.forEach(function(selectedFile) {
-            if (ingestedFile === selectedFile) {
-                ingested_files.splice(ingestedIndex, 1);
-            }
-        });
-    });
-    setIngestedFiles(ingested_files);
-}
-
-
-async function deleteFile(doc_ids) {
+async function deleteFile(file_name, deleting) {
     try {
-        const deletePromises = doc_ids.map(async (doc_id) => {
-            const response = await fetch(`http://${url}/v1/ingest/${doc_id}`, {
-                method: 'DELETE'
-            });
-
-            if (!response.ok) {
-                throw new Error(`Error al eliminar el archivo ${doc_id}: ${response.status}`);
-            }
-
-            console.log(`Archivo "${doc_id}" eliminado correctamente.`);
+        const response = await fetch(`http://${url}/v1/ingest/${file_name}`, {
+            method: 'DELETE'
         });
 
-        // Espera a que todas las promesas se resuelvan (es decir, todas las solicitudes DELETE se completen)
-        await Promise.all(deletePromises);
+        if (response.ok) {
+            getIngestedFiles();
+            deleting.style.display = 'none';
+        } else {
+            deleting.style.display = 'none';
+            throw new Error(`Error al eliminar el archivo ${doc_id}: ${response.status}`);
+        }
+
     } catch (error) {
         console.error('Error al procesar la solicitud de eliminar:', error);
     }
@@ -325,26 +258,17 @@ async function deleteFile(doc_ids) {
 
 
 async function send() {
-    reset
-    var input = document.getElementById('input').value.trim();
+    var input = document.getElementById('input');
+    var value = input.value.trim();
     var display = document.getElementById('chat');
-    console.log('input', input);
 
-    if (input === '') {
-        display.innerHTML = 'Escribe papi';
-    } else {
+    if (value !== '') {
         try {
             const requestBody = {
-                text: input,
-                context_filter: {
-                    docs_ids: [
-                        '6f842806-9c28-4abb-a5ef-4f01c5c5ed12'
-                    ]
-                },
+                text: value,
                 limit: 10,
-                prev_next_chunks: 2
+                prev_next_chunks: 100
             };
-            console.log('Request body:', requestBody);
 
             const response = await fetch(`http://${url}/v1/chunks`, {
                 method: 'POST',
@@ -358,28 +282,49 @@ async function send() {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
-            const responseData = await response.json();
-            responseData.data.forEach(chunk => {
+            var responseData = await response.json();
+            var messages = '';
+            var count = 1;
+            responseData.data.forEach((chunk, idx) => {
                 var windowText = chunk.document.doc_metadata.window;
                 var originalText = chunk.document.doc_metadata.original_text;
                 var text = chunk.text;
                 var previousTexts = chunk.previous_texts;
                 var nextTexts = chunk.next_texts;
                 var file = chunk.document.doc_metadata.file_name;
-
-                console.log('Window Text:', windowText);
-                console.log('Original Text:', originalText);
-                console.log('Text:', text);
-                console.log('Previous Texts:', previousTexts);
-                console.log('Next Texts:', nextTexts);
-                var mensage = `
-                               Texto: ${text}
-                               </br>
-                               Fuente: ${file}
-                               </br></br>
-                               `
-                display.innerHTML += mensage;
+                var page = chunk.document.doc_metadata.page_label;
+                var message;
+                // console.log('Window Text:', windowText);
+                // console.log('Original Text:', originalText);
+                // console.log('Text:', text);
+                // console.log('Previous Texts:', previousTexts);
+                // console.log('Next Texts:', nextTexts);
+                if (comprobarContenido(value, text)) {
+                    if (page === undefined) {
+                        message = `
+                                   ${count} . <b>${file}</b>
+                                   </br>
+                                   Texto: ${text}
+                                   </br></br>`
+                    } else {
+                        message = `
+                                    ${count} . <b>${file} (pagina ${page})</b>
+                                   </br>
+                                   Texto: ${text}
+                                   </br></br>`
+                    }
+                    
+                    messages += message;
+                    count++;
+                }
             });
+            console.log('server message: ', messages);
+            display.innerHTML += "<div class='border bg-primary text-light p-2 ml-auto message mt-2 mb-2'>" + value + "</div>";                        
+            if (messages !== '') {
+                display.innerHTML += "<div class='border bg-dark text-light p-2 message'>" + messages + "</div>";
+                }
+            display.scrollTop = display.scrollHeight;
+            input.value = '';
 
         } catch (error) {
             console.error('There was an error with the fetch request:', error);
@@ -388,9 +333,70 @@ async function send() {
 }
 
 
+var deleteVisible = false;
+
+function confirmDelete() {
+    var del = document.getElementById('delete');
+    var conf = document.getElementById('confirm-delete');
+
+    if (!deleteVisible) {
+        del.style.display = 'none';
+        conf.style.display = 'flex';
+    } else {
+        del.style.display = 'flex';
+        conf.style.display = 'none';
+    }
+    deleteVisible = !deleteVisible;
+}
+
+
+
+function comprobarContenido(cadena1, cadena2) {
+    var server_words = cadena2.split(' ');
+    var user_words = cadena1.split(' ');
+    var encontrada = false;
+
+    user_words.forEach(function (palabra, index) {
+        user_words[index] = processText(palabra);
+    });
+
+    server_words.forEach(function (palabra) {
+        var server = processText(palabra);
+        var user = processText(cadena1);
+
+        console.log(server, ' &&  ', user)
+        console.log(user_words);
+        if (user_words.includes(server)) {
+            console.log('conincidencia: ', palabra, 'CON :', cadena1)
+            console.log('EN: ', server_words)
+            encontrada = true;
+        }
+    });
+    return encontrada;
+}
+
+
+function processText(cadena) {
+    cadena = cadena.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    cadena = cadena.replace(/[.,()'…":\[\]\n—]/g, '');
+    cadena = cadena.toLowerCase();
+
+    return cadena;
+}
+
+
 function reset() {
     var display = document.getElementById('chat');
+    var input = document.getElementById('input');
     display.innerHTML = '';
+    input.value = '';
+}
+
+
+function handleKeyPress(event) {
+    if (event.key === "Enter") {
+        send();
+    }
 }
 
 
@@ -408,9 +414,6 @@ function seeIngested() {
         console.log(ingested_files[i]);
     }
 }
-
-
-console.log(relations)
 
 setInterval(keepAlive, 10000);
 getIngestedFiles();
